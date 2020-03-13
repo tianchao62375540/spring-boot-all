0e658ab44865f7026bbea2f179018b704cd87aff
tc_tc_tc <455433520@qq.com>
2020-03-13 20:00:52 +0800

    rabbitmq

---
+++ spring-boot-redis/src/main/java/com/tc/RedisApplication.java
@@ -0,0 +1,16 @@
+package com.tc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: tianchao
+ * @Date: 2020/3/11 20:18
+ * @Description:
+ */
+@SpringBootApplication
+public class RedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+}
